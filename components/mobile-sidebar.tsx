8ae8090383eefc9ca202f0c3e2fec65fa5dd7fd6
Piotr Rzadkowolski <p.rzadkowolski@auroracreation.com>
2024-01-23 13:13:38 +0100

    add mobile sidebar content

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,5 +1,33 @@
-const MobileSidebar = () => {
-  return <div>MobileSidebar</div>;
-};
+"use client";
 
-export default MobileSidebar;
+import { useEffect, useState } from "react";
+import { Menu } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Sidebar } from "@/components/sidebar";
+
+export const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
