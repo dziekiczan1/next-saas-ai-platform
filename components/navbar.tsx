5799b8fc3e6356eb2b5537acd72fe846666991e9
Piotr Rzadkowolski <p.rzadkowolski@auroracreation.com>
2024-01-23 13:31:13 +0100

    add heading for dashboard page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,6 @@
 import { UserButton } from "@clerk/nextjs";
 
-import MobileSidebar from "@/components/mobile-sidebar";
+import { MobileSidebar } from "@/components/mobile-sidebar";
 
 const Navbar = async () => {
   return (
