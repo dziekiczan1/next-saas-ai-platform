46c289b6b4b6428f67fbe471f62eb283c4184d74
dziekiczan1 <p.rzadkowolski@gmail.com>
2024-01-29 13:06:30 +0100

    add new variant to button component

--- components/free-counter.tsx
+++ components/free-counter.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 
 import { MAX_FREE_COUNTS } from "@/constants";
 import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Progress } from "@/components/ui/progress";
 import { SidebarProps } from "./sidebar";
 
 export const FreeCounter = ({ apiLimitCount = 0 }: SidebarProps) => {
@@ -24,7 +26,15 @@ export const FreeCounter = ({ apiLimitCount = 0 }: SidebarProps) => {
             <p>
               {apiLimitCount} / {MAX_FREE_COUNTS} Free Generations
             </p>
+            <Progress
+              className="h-3"
+              value={(apiLimitCount / MAX_FREE_COUNTS) * 100}
+            />
           </div>
+          <Button variant="premium" className="w-full">
+            Upgrade
+            <Zap className="w-4 h-4 ml-2 fill-white" />
+          </Button>
         </CardContent>
       </Card>
     </div>
