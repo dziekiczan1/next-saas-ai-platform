be24c53e1351e4cc146aa8f847b8b26c8b48ecdf
dzekiczan1 <p.rzadkowolski@gmail.com>
2024-02-03 14:22:43 +0100

    install crisp chat package

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -3,6 +3,7 @@
 import axios from "axios";
 import { useState } from "react";
 import { Check, Zap } from "lucide-react";
+import { toast } from "react-hot-toast";
 
 import {
   Dialog,
@@ -30,7 +31,7 @@ export const ProModal = () => {
 
       window.location.href = response.data.url;
     } catch (error) {
-      console.log(error);
+      toast.error("Something went wrong");
     } finally {
       setLoading(false);
     }
